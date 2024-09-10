4cd809ff85000c82c0cc3777f2bb3214448aec86
SheikhMuhammadYaseen <satisfyingmode@gmail.com>
2024-09-10 21:09:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-var toggleSkillsButton = document.getElementById("toggleAll");
-var sections = ["education", "skills", "work-experience"];
-toggleSkillsButton.addEventListener("click", function () {
-    sections.forEach(function (sectionId) {
+var toggleAllButton = document.getElementById("toggleAll");
+var sectionsToToggle = ["skills", "education", "work-experience"];
+toggleAllButton.addEventListener("click", function () {
+    var areSectionsHidden = false;
+    sectionsToToggle.forEach(function (sectionId) {
         var section = document.getElementById(sectionId);
-        if (section) {
-            if (section.style.display === "none" || section.style.display === "") {
-                section.style.display = "block";
-            }
-            else {
-                section.style.display = "none";
-            }
+        if (section && (section.style.display === "none" || section.style.display === "")) {
+            areSectionsHidden = true;
         }
     });
+    sectionsToToggle.forEach(function (sectionId) {
+        var section = document.getElementById(sectionId);
+        if (section) {
+            section.style.display = areSectionsHidden ? "block" : "none";
+        }
+    });
+    toggleAllButton.textContent = areSectionsHidden ? "Hide All Sections" : "Show All Sections";
 });
